45cb32905324e845111c5e7b5ecbbdc3dd4cc918
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 14:16:05 +0530

    Update Pattern5.java

--- 1-basics-of-programming/2-patterns/Pattern5.java
+++ 1-basics-of-programming/2-patterns/Pattern5.java
@@ -5,30 +5,41 @@ public class Pattern5 {
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
+
         int nst = 1;
         int nsp = n / 2;
 
-        for (int i = 1; i <= n; i++) {
+        for (int i = 1; i <= n; i++) { // loop to control rows
 
-            for (int j = 1; j <= nsp; j++) {
-                System.out.print("	");
+            for (int k = 1; k <= nsp; k++) { // controls spaces
+                System.out.print("\t");
             }
 
-            for (int j = 1; j <= nst; j++) {
-                System.out.print("*	");
+            for (int j = 1; j <= nst; j++) { // controls stars
+                System.out.print("*\t");
             }
 
+            // updation of stars and spaces before changing row
             if (i <= n / 2) {
-                nst += 2;
                 nsp--;
+                nst += 2;
             } else {
-                nst -= 2;
                 nsp++;
+                nst -= 2;
             }
-
+            // changing line in console
             System.out.println();
-
         }
-
     }
 }
+
+
+// 		    *	
+// 	    *	*	*	
+// *	*	*	*	*	
+// 	    *	*	*	
+// 		    *	
