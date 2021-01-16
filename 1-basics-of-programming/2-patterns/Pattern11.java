0c39667bfdec06e8a324d9db8ac4dbd7869010d7
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 16:28:32 +0530

    Update Pattern11.java

--- 1-basics-of-programming/2-patterns/Pattern11.java
+++ 1-basics-of-programming/2-patterns/Pattern11.java
@@ -4,23 +4,25 @@ public class Pattern11 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
-        int nsp = 0;
-        int val = 1;
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
+        int count = 1;
         for (int i = 1; i <= n; i++) {
-
             for (int j = 1; j <= i; j++) {
-                System.out.print(val + "	");
-                val++;
+                System.out.print(count + "\t");
+                count++;
             }
-
-            for (int j = 1; j <= nsp; j++) {
-                System.out.print("	");
-            }
-
             System.out.println();
         }
-
     }
 }
+
+
+// 1	
+// 2	3	
+// 4	5	6	
+// 7	8	9	10	
+// 11	12	13	14	15	
