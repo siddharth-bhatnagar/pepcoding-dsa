3620662a67d7fa401a85387619e50c62546752f2
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 14:51:32 +0530

    Update Pattern9.java

--- 1-basics-of-programming/2-patterns/Pattern9.java
+++ 1-basics-of-programming/2-patterns/Pattern9.java
@@ -1,43 +1,54 @@
 import java.util.*;
 
-public class Pattern9 {
+public class Main {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
-        int nsp1 = 0;
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
         int nst = 1;
+        int nsp1 = 0;
         int nsp2 = n - 2;
+        for (int i = 1; i <= n; i++) { // controls rows
 
-        for (int i = 1; i <= n; i++) {
-
-            for (int j = 1; j <= nsp1; j++) {
-                System.out.print("	");
+            for (int j = 1; j <= nsp1; j++) { // controls spaces 1
+                System.out.print("\t");
             }
 
-            for (int j = 1; j <= nst; j++) {
-                System.out.print("*	");
+            for (int j = 1; j <= nst; j++) { // controls stars 1
+                System.out.print("*\t");
             }
 
-            for (int j = 1; j <= nsp2; j++) {
-                System.out.print("	");
+            for (int j = 1; j <= nsp2; j++) { // controls spaces 2
+                System.out.print("\t");
             }
-
-            if (i != n / 2 + 1) {
-                for (int j = 1; j <= nst; j++) {
-                    System.out.print("*	");
+            if (i != (n / 2) + 1) {
+                for (int j = 1; j <= nst; j++) { // controls stars 2
+                    System.out.print("*\t");
                 }
             }
 
-            if (i <= n / 2) {
+
+            if (i <= n / 2) { // updations to stars and spaces in each row
                 nsp1++;
                 nsp2 -= 2;
             } else {
                 nsp1--;
                 nsp2 += 2;
             }
+            // changing line in console
             System.out.println();
         }
     }
 }
+
+
+
+// *				*	
+// 	    *		*		
+// 	    	*			
+// 	    *		*		
+// *				*	
