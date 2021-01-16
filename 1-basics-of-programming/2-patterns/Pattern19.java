826402e7ec6d622ce6f47521ab4ce35ea4b977b8
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 20:19:36 +0530

    Update Pattern19.java

--- 1-basics-of-programming/2-patterns/Pattern19.java
+++ 1-basics-of-programming/2-patterns/Pattern19.java
@@ -4,32 +4,37 @@ public class Pattern19 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
 
         for (int i = 1; i <= n; i++) {
-
             for (int j = 1; j <= n; j++) {
-
-                if (i == 1 && (j <= n / 2 + 1 || j == n)) {
-                    System.out.print("*	");
-                }
-
-                else if ((i > 1 && i <= n / 2) && (j == n / 2 + 1 || j == n)) {
-                    System.out.print("*	");
-                } else if ((i == n / 2 + 1) && (j >= 1 || j <= n)) {
-                    System.out.print("*	");
-                } else if ((i > n / 2 + 1 && i < n) && (j == 1 || j == n / 2 + 1)) {
-                    System.out.print("*	");
-                } else if ((i == n) && (j == 1 || j >= n / 2 + 1)) {
-                    System.out.print("*	");
-                } else {
-                    System.out.print("	");
-                }
+                if ((i==1) && (j<=n/2+1 || j==n))
+                    System.out.print("*\t");
+                else if((i>1&&i<=n/2) && (j==n/2+1 || j==n))
+                    System.out.print("*\t");
+                else if((i==n/2+1) && (j>=1&&j<=n))
+                    System.out.print("*\t");
+                else if((i>n/2+1&&i<n) && (j==1 || j==n/2+1))
+                    System.out.print("*\t");
+                else if(i==n && (j==1 || j>=n/2+1))
+                    System.out.print("*\t");
+                else 
+                    System.out.print("\t");
             }
-
             System.out.println();
         }
-
     }
 }
+
+
+// *	*	*	*			*	
+// 			    *			*	
+// 		    	*			*	
+// *	*	*	*	*	*	*	
+// *			*				
+// *			*				
+// *			*	*	*	*	
