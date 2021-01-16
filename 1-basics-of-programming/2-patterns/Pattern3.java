fef24a8a7591931a8291c343d0193419e1049003
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 14:02:16 +0530

    Update Pattern3.java

--- 1-basics-of-programming/2-patterns/Pattern3.java
+++ 1-basics-of-programming/2-patterns/Pattern3.java
@@ -4,27 +4,34 @@ public class Pattern3 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
-        // write ur code here
-        int nst = n;
-        int nsp = 0;
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
+
+        int nst = 1;
+        int nsp = n - 1;
 
         for (int i = 1; i <= n; i++) {
 
+            for (int k = 1; k <= nsp; k++) {
+                System.out.print("\t");
+            }
+            nsp--;
+
             for (int j = 1; j <= nst; j++) {
-                System.out.print("*	");
+                System.out.print("*\t");
             }
+            nst++;
 
-            for (int j = 1; j <= nsp; j++) {
-                System.out.print("	");
-            }
-
-            nst--;
-            nsp++;
             System.out.println();
-
         }
-
     }
 }
+
+//     			    *	
+//     		    *	*	
+//     	    *	*	*	
+//     *	*	*	*	
+// *	*	*	*	*	
