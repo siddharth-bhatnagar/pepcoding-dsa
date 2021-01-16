4a631c5f7178868fe0177896a73dd60605adcf72
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 14:26:47 +0530

    Update Pattern8.java

--- 1-basics-of-programming/2-patterns/Pattern8.java
+++ 1-basics-of-programming/2-patterns/Pattern8.java
@@ -4,23 +4,33 @@ public class Pattern8 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
-        int nsp = n - 1;
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
         int nst = 1;
+        int nsp = n-1;
 
         for (int i = 1; i <= n; i++) {
+            
             for (int j = 1; j <= nsp; j++) {
-                System.out.print("	");
+                System.out.print("\t");
             }
-
+            nsp--;    
+            
             for (int j = 1; j <= nst; j++) {
-                System.out.print("*	");
+                System.out.print("*\t");
             }
 
-            nsp--;
             System.out.println();
         }
-
     }
 }
+
+
+// 				    *	
+// 			    *		
+// 		    *			
+// 	    *				
+//  *
