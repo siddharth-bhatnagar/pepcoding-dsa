5c32d3e54f912e7a32d8c65e111ed3bb18866eba
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 14:24:56 +0530

    Update Pattern7.java

--- 1-basics-of-programming/2-patterns/Pattern7.java
+++ 1-basics-of-programming/2-patterns/Pattern7.java
@@ -4,25 +4,33 @@ public class Pattern7 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
         int nst = 1;
         int nsp = 0;
 
         for (int i = 1; i <= n; i++) {
-
+            
             for (int j = 1; j <= nsp; j++) {
-                System.out.print("	");
+                System.out.print("\t");
             }
-
+            nsp++;    
+            
             for (int j = 1; j <= nst; j++) {
-                System.out.print("*	");
+                System.out.print("*\t");
             }
 
-            nsp++;
             System.out.println();
-
         }
-
     }
 }
+
+
+// *	
+//      *	
+// 	    	*	
+// 		    	*	
+// 			    	*	
