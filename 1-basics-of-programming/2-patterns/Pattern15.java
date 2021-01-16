54edd6b240c63e4e5912526f7ab0c6ef36ec1e81
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 17:37:21 +0530

    Update Pattern15.java

--- 1-basics-of-programming/2-patterns/Pattern15.java
+++ 1-basics-of-programming/2-patterns/Pattern15.java
@@ -4,20 +4,26 @@ public class Pattern15 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
+
+        int nst = 1;
         int nsp = n / 2;
         int val = 1;
-        int nst = 1;
 
         for (int i = 1; i <= n; i++) {
             int xval = val;
+
             for (int j = 1; j <= nsp; j++) {
-                System.out.print("	");
+                System.out.print("\t");
             }
 
             for (int j = 1; j <= nst; j++) {
-                System.out.print(xval + "	");
+                System.out.print(xval + "\t");
+
                 if (j <= nst / 2) {
                     xval++;
                 } else {
@@ -30,12 +36,32 @@ public class Pattern15 {
                 nst += 2;
                 val++;
             } else {
-                val--;
                 nsp++;
                 nst -= 2;
+                val--;
             }
+
             System.out.println();
         }
-
     }
 }
+
+
+
+// 	    	1	
+// 	    2	3	2	
+//  3	4	5	4	3	
+//  	2	3	2	
+// 		    1	
+
+
+
+
+
+
+
+
+
+
+
+
