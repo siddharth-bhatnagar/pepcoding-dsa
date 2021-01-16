0360888447c367080b38d01eabd4d2c4772b6c3c
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 18:22:52 +0530

    Update Pattern16.java

--- 1-basics-of-programming/2-patterns/Pattern16.java
+++ 1-basics-of-programming/2-patterns/Pattern16.java
@@ -1,38 +1,37 @@
 import java.util.*;
 
 public class Pattern16 {
+
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
         int n = scn.nextInt();
+        pattern(n);
+    }
 
-        int nsp = n * 2 - 3;
+    public static void pattern(int n) {
         int nst = 1;
-
+        int nsp = 2 * n - 3;
         for (int i = 1; i <= n; i++) {
             int val = 1;
-            int x = i;
             for (int j = 1; j <= nst; j++) {
-                System.out.print(val + "	");
+                System.out.print(val + "\t");
                 val++;
             }
-
             for (int j = 1; j <= nsp; j++) {
-                System.out.print("	");
+                System.out.print("\t");
             }
-
             if (i < n) {
-
+                int xval = i;
                 for (int j = 1; j <= nst; j++) {
-                    System.out.print(x + "	");
-                    x--;
+                    System.out.print(xval + "\t");
+                    xval--;
                 }
             } else {
-                x = x - 1;
+                int xval = i - 1;
                 for (int j = 1; j < nst; j++) {
-                    System.out.print(x + "	");
-                    x--;
+                    System.out.print(xval + "\t");
+                    xval--;
                 }
-
             }
             nsp -= 2;
             nst++;
@@ -40,3 +39,21 @@ public class Pattern16 {
         }
     }
 }
+
+
+// 1												1	
+// 1	2										2	1	
+// 1	2	3								3	2	1	
+// 1	2	3	4						4	3	2	1	
+// 1	2	3	4	5				5	4	3	2	1	
+// 1	2	3	4	5	6		6	5	4	3	2	1	
+// 1	2	3	4	5	6	7	6	5	4	3	2	1	
+
+
+
+
+
+
+
+
+
