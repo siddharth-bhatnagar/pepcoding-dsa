b41a844a10baffe9fdca46168e5125d7ce1c28c9
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 13:57:03 +0530

    Update Pattern2.java

--- 1-basics-of-programming/2-patterns/Pattern2.java
+++ 1-basics-of-programming/2-patterns/Pattern2.java
@@ -4,27 +4,29 @@ public class Pattern2 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
-        // write ur code here
+
         int nst = n;
         int nsp = 0;
 
         for (int i = 1; i <= n; i++) {
 
             for (int j = 1; j <= nst; j++) {
-                System.out.print("*	");
+                System.out.print("*\t");
             }
-
-            for (int j = 1; j <= nsp; j++) {
-                System.out.print("	");
-            }
-
             nst--;
+            for (int k = 1; k <= nsp; k++) {
+                System.out.print("\t");
+            }
             nsp++;
             System.out.println();
-
         }
-
     }
 }
+
+
+// *	*	*	*	*	
+// *	*	*	*		
+// *	*	*			
+// *	*				
+// *	
