f2210a8a04a3a7575734cb7e1b9c558b562234b4
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 20:40:37 +0530

    Update Pattern20.java

--- 1-basics-of-programming/2-patterns/Pattern20.java
+++ 1-basics-of-programming/2-patterns/Pattern20.java
@@ -4,24 +4,31 @@ public class Pattern20 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
 
         for (int i = 1; i <= n; i++) {
-
             for (int j = 1; j <= n; j++) {
-
-                if (j == 1 || j == n) {
-                    System.out.print("*	");
-                } else if (i > n / 2 && (i == j || i + j == n + 1)) {
-                    System.out.print("*	");
-                } else {
-                    System.out.print("	");
-                }
-
+                if(j==1||j==n)
+                    System.out.print("*\t");
+                else if((i+j==n+1 || i==j) && i>n/2)
+                    System.out.print("*\t");
+                else 
+                    System.out.print("\t");
             }
 
             System.out.println();
         }
     }
 }
+
+
+
+// *				*	
+// *				*	
+// *		*		*	
+// *	*		*	*	
+// *				*	
