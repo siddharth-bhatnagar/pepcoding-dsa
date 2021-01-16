e4d66a1738934b62833e604312e6701d2c4f4872
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 16:34:47 +0530

    Update Pattern12.java

--- 1-basics-of-programming/2-patterns/Pattern12.java
+++ 1-basics-of-programming/2-patterns/Pattern12.java
@@ -4,20 +4,29 @@ public class Pattern12 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
-        int a = 0, b = 1, nextnum = 0;
-        for (int i = 1; i <= n; i++) {
+        pattern(n);
+    }
 
+    public static void pattern(int n) {
+        int a = 0;
+        int b = 1;
+        for (int i = 1; i <= n; i++) {
             for (int j = 1; j <= i; j++) {
-                System.out.print(a + "	");
-                nextnum = a + b;
+                System.out.print(a + "\t");
+                int nextnum = a + b;
                 a = b;
                 b = nextnum;
             }
-
             System.out.println();
         }
-
     }
 }
+
+
+
+// 0	
+// 1	1	
+// 2	3	5	
+// 8	13	21	34	
+// 55	89	144	233	377	
