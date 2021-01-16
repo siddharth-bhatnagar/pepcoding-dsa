05326afdf7b24925cc698b72d2e04b7efbe76bbb
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 13:43:38 +0530

    modified pattern1

--- 1-basics-of-programming/2-patterns/Pattern1.java
+++ 1-basics-of-programming/2-patterns/Pattern1.java
@@ -4,20 +4,45 @@ public class Pattern1 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
-        int nst = 1;
-        int nsp = 0;
+
+        // method1(n);
+        method2(n);
+    }
+
+    public static void method1(int n) {
         for (int i = 1; i <= n; i++) {
-            for (int j = 1; j <= nst; j++) {
-                System.out.print("*	");
+            for (int j = 1; j <= i; j++) {
+                System.out.print("*\t");
             }
-            for (int j = 1; j <= nsp; j++) {
-                System.out.print("	");
-            }
-            nst++;
             System.out.println();
         }
+    }
 
+    public static void method2(int n) {
+        int nst = 1;
+        int nsp = n - 1;
+        for (int i = 1; i <= n; i++) { // this loop controls the rows
+
+            for (int j = 1; j <= nst; j++) { // This loop prints stars in each row iteratively
+                System.out.print("*\t");
+            }
+            nst++; // incrementing total star for each row
+
+            for (int j = 1; j <= nsp; j++) { // It handles spaces in each row iteratively
+                System.out.print("\t");
+            }
+            nsp--; // incrementing total spaces in each row    
+
+            System.out.println(); // to change line before changing row
+        }
     }
 }
+
+    // The code above prints the pattern:
+
+    // *	
+    // *	*	
+    // *	*	*	
+    // *	*	*	*	
+    // *	*	*	*	*	
