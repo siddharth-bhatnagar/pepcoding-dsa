54037076a30f38bab8bc6d39906f52486f2599b4
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 17:05:57 +0530

    Update Pattern14.java

--- 1-basics-of-programming/2-patterns/Pattern14.java
+++ 1-basics-of-programming/2-patterns/Pattern14.java
@@ -4,11 +4,24 @@ public class Pattern14 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        table(n);
+    }
 
-        int x = scn.nextInt();
+    public static void table(int n) {
         for (int i = 1; i <= 10; i++) {
-            System.out.println(x + " * " + i + " = " + (x * i));
+            System.out.println(n + " * " + i + " = " + (n * i));
         }
-
     }
 }
+// for n = 3
+// 3 * 1 = 3
+// 3 * 2 = 6
+// 3 * 3 = 9
+// 3 * 4 = 12
+// 3 * 5 = 15
+// 3 * 6 = 18
+// 3 * 7 = 21
+// 3 * 8 = 24
+// 3 * 9 = 27
+// 3 * 10 = 30
