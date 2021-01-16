648bea2128827a8549d33c79f448d1fd60ed47ec
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 14:04:48 +0530

    Update Pattern4.java

--- 1-basics-of-programming/2-patterns/Pattern4.java
+++ 1-basics-of-programming/2-patterns/Pattern4.java
@@ -4,26 +4,34 @@ public class Pattern4 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
+        pattern(n);
+    }
+
+    public static void pattern(int n) {
+
         int nst = n;
         int nsp = 0;
 
         for (int i = 1; i <= n; i++) {
 
-            for (int j = 1; j <= nsp; j++) {
-                System.out.print("	");
+            for (int k = 1; k <= nsp; k++) {
+                System.out.print("\t");
             }
+            nsp++;
 
             for (int j = 1; j <= nst; j++) {
-                System.out.print("*	");
+                System.out.print("*\t");
             }
-
             nst--;
-            nsp++;
+
             System.out.println();
-
         }
-
     }
 }
+
+// *	*	*	*	*	
+// 	    *	*	*	*	
+// 		    *	*	*	
+// 		        *	*	
+// 				    *
