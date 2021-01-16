a6a04818ae9f1b9abbc912804a359a8c5af7cb22
Siddharth Bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-16 17:01:45 +0530

    Update Pattern13.java

--- 1-basics-of-programming/2-patterns/Pattern13.java
+++ 1-basics-of-programming/2-patterns/Pattern13.java
@@ -4,17 +4,27 @@ public class Pattern13 {
 
     public static void main(String[] args) {
         Scanner scn = new Scanner(System.in);
-
         int n = scn.nextInt();
-        for (int i = 0; i < n; i++) {
-            int iCj = 1;
-            for (int j = 0; j <= i; j++) {
+        pattern(n);
+    }
 
-                System.out.print(iCj + "	");
-                int iCjplus1 = iCj * (i - j) / (j + 1);
-                iCj = iCjplus1;
+    public static void pattern(int n) {
+        for(int i=0;i<n;i++){
+            int icj = 1;
+            for(int j=0;j<=i;j++){
+                System.out.print(icj+"\t");
+                int icjp1 = (icj*(i-j))/(j+1);
+                icj = icjp1;
             }
             System.out.println();
         }
     }
 }
+
+
+
+// 1	
+// 1	1	
+// 1	2	1	
+// 1	3	3	1	
+// 1	4	6	4	1
