2e3dbb5d79ccbd2c6722a5332a5147b1bf6ccad4
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-17 13:50:56 +0530

    added 2d array solutions part1

---
+++ 1-basics-of-programming/4-2d-arrays/MatrixMultiplication.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class MatrixMultiplication {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int r1 = scn.nextInt();
+        int c1 = scn.nextInt();
+        int a[][] = new int[r1][c1];
+        for (int i = 0; i < a.length; i++) {
+            for (int j = 0; j < a[0].length; j++) {
+                a[i][j] = scn.nextInt();
+            }
+        }
+
+        int r2 = scn.nextInt();
+        int c2 = scn.nextInt();
+        int b[][] = new int[r2][c2];
+        for (int i = 0; i < b.length; i++) {
+            for (int j = 0; j < b[0].length; j++) {
+                b[i][j] = scn.nextInt();
+            }
+        }
+
+        if (c1 == r2) {
+            printProduct(a, b);
+        } else {
+            System.out.println("Invalid input");
+        }
+    }
+
+    public static void printProduct(int[][] a, int[][] b) {
+        int result[][] = new int[a.length][b[0].length];
+        for (int i = 0; i < result.length; i++) {
+            for (int j = 0; j < result[0].length; j++) {
+                for (int k = 0; k < b.length; k++) {
+                    result[i][j] += a[i][k] * b[k][j];
+                }
+            }
+        }
+        display2DArray(result);
+    }
+
+    public static void display2DArray(int[][] arr) {
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                System.out.print(arr[i][j] + " ");
+            }
+            System.out.println();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
---
+++ 1-basics-of-programming/4-2d-arrays/SpiralDisplay.java
@@ -0,0 +1,62 @@
+import java.io.*;
+import java.util.*;
+
+public class SpiralDisplay {
+
+    public static void main(String[] args) throws Exception {
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int m = scn.nextInt();
+
+        int[][] arr = new int[n][m];
+        for (int i = 0; i < arr.length; i++) {
+            for (int j = 0; j < arr[0].length; j++) {
+                arr[i][j] = scn.nextInt();
+            }
+        }
+
+        spiralDisplay(n, m, arr);
+    }
+
+    public static void spiralDisplay(int r, int c, int[][] arr) {
+
+        int maxr = r - 1;
+        int maxc = c - 1;
+        int minr = 0;
+        int minc = 0;
+        int total = r * c;
+        int count = 0;
+        while (count < total) {
+
+            // Left Wall
+            for (int i = minr; i <= maxr && count < total; i++) {
+                System.out.println(arr[i][minc]);
+                count++;
+            }
+            minc++;
+
+            // Bottom Wall
+            for (int i = minc; i <= maxc && count < total; i++) {
+                System.out.println(arr[maxr][i]);
+                count++;
+            }
+            maxr--;
+
+            // Right Wall
+            for (int i = maxr; i >= minr && count < total; i--) {
+                System.out.println(arr[i][maxc]);
+                count++;
+            }
+            maxc--;
+
+            // Upper Wall
+            for (int i = maxc; i >= minc && count < total; i--) {
+                System.out.println(arr[minr][i]);
+                count++;
+            }
+            minr++;
+        }
+
+    }
+
+}
