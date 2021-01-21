29ca578497125080a6ebe8fffc5ba760dfc8cc33
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-21 17:29:52 +0530

    updated stock span

--- 4-basic-data-structures/1-stacks-and-queues/StockSpan.java
+++ 4-basic-data-structures/1-stacks-and-queues/StockSpan.java
@@ -25,23 +25,31 @@ public class StockSpan {
     }
 
     public static int[] solve(int[] arr) {
-        int[] ans = new int[arr.length];
+        int ans[] = new int[arr.length];
         Stack < Integer > stack = new Stack < > ();
-
         for (int i = arr.length - 1; i >= 0; i--) {
-
-            while (stack.size() != 0 && arr[i] > arr[stack.peek()]) {
-                int solvedidx = stack.pop();
-                ans[solvedidx] = solvedidx - i;
+            while (stack.size() > 0 && arr[i] > arr[stack.peek()]) {
+                int si = stack.pop();
+                ans[si] = si - i;
             }
-
             stack.push(i);
-
         }
-        while (stack.size() != 0) {
-            int idx = stack.pop();
-            ans[idx] = idx + 1;
+        while (stack.size() > 0) {
+            int usi = stack.pop();
+            ans[usi] = usi + 1;
         }
         return ans;
     }
 }
+
+
+// This question requires tweaking NGE on left according to what is asked.
+
+/*
+--------------------------------Approach---------------------------------
+
+Find the next greatest element to the left in any and find the difference between the current value of i
+and the next greatest element on left's index in the given array and store it in ans array.
+if NGE is not present, simply place i+1 in the place of that element in the ans array.
+
+**/
