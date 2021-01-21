3972fe756127f77a44647bba1295a3451f3ac1b4
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-21 13:49:35 +0530

    updated duplicate brackets

--- 4-basic-data-structures/1-stacks-and-queues/DuplicateBrackets.java
+++ 4-basic-data-structures/1-stacks-and-queues/DuplicateBrackets.java
@@ -6,13 +6,16 @@ public class DuplicateBrackets {
     public static void main(String[] args) throws Exception {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = new String(br.readLine());
-        Stack<Character> stack = new Stack<>();
+        duplicateBrackets(str);
+    }
+
+    public static void duplicateBrackets(String str) {
+        Stack < Character > stack = new Stack < > ();
         for (int i = 0; i < str.length(); i++) {
             char ch = str.charAt(i);
             if (ch == ')') {
                 if (stack.peek() == '(') {
-                    System.out.println("true");
-                    stack.pop();
+                    System.out.println(true);
                     return;
                 } else {
                     while (stack.peek() != '(') {
@@ -24,6 +27,16 @@ public class DuplicateBrackets {
                 stack.push(ch);
             }
         }
-        System.out.println("false");
+        System.out.println(false);
     }
 }
+
+/*
+Assumption - the given expression is balanced.
+Approach
+
+if stack is empty, push element in stack till first closing bracket is encountered.
+peek at the top of stack, if top = opening bracket, then there are duplicates else 
+keep popping till first opening bracket is found, then pop that as well.
+
+**/
