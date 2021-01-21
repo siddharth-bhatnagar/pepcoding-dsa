963f918f480a76d536806a27fd1a39d3c5c170a9
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-21 16:41:25 +0530

    changed class name in NSE on right to the correct one

--- 4-basic-data-structures/1-stacks-and-queues/NGEonRight.java
+++ 4-basic-data-structures/1-stacks-and-queues/NGEonRight.java
@@ -1,7 +1,7 @@
 import java.io.*;
 import java.util.*;
 
-public class Main {
+public class NSEonRight {
     public static void display(int[] a) {
         StringBuilder sb = new StringBuilder();
 
