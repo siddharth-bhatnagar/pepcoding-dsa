033fe546aa17f31f6057a6b54eac2a94c9c8cec7
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-30 21:42:18 +0530

    added code for printing subsequence of string recursively

---
+++ 2-recursion-and-backtracking/4-recursion-on-the-way-up/printSubsequence.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class printSubsequence {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        printSS(str, "");
+    }
+
+    public static void printSS(String str, String ans) {
+        if (str.length() == 0) {
+            System.out.println(ans);
+            return;
+        }
+
+        char fc = str.charAt(0);
+        String ros = str.substring(1);
+        
+        printSS(ros, ans + fc);
+        printSS(ros, ans + "");
+    }
+}
