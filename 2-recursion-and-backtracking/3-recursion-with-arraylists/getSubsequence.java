e183e342d1e73f3f82851cf313a0d1b3a6fd800d
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-01-30 21:28:58 +0530

    added code for get subsequence

---
+++ 2-recursion-and-backtracking/3-recursion-with-arraylists/getSubsequence.java
@@ -0,0 +1,35 @@
+import java.io.*;
+import java.util.*;
+
+public class getSubsequence {
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println(gss(str));
+    }
+
+    public static ArrayList < String > gss(String str) {
+
+        if (str.length() == 0) {
+            ArrayList < String > temp = new ArrayList < > ();
+            temp.add("");
+            return temp;
+        }
+
+        char fc = str.charAt(0);
+        String ros = str.substring(1);
+        ArrayList < String > smallans = gss(ros);
+        ArrayList < String > result = new ArrayList < > ();
+
+        for (int i = 0; i < smallans.size(); i++) {
+            result.add("" + smallans.get(i));
+        }
+
+        for (int i = 0; i < smallans.size(); i++) {
+            result.add(fc + smallans.get(i));
+        }
+
+        return result;
+    }
+}
