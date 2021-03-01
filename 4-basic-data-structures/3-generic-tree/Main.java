9991b7c4fe05537d3da254a17db750af6c0be78d
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-03-01 14:00:57 +0530

    gt updated

--- 4-basic-data-structures/3-generic-tree/Main.java
+++ 4-basic-data-structures/3-generic-tree/Main.java
@@ -1,59 +1,71 @@
-import java.util.*;
+// Introduction to hierarchical Data Structures
+// Examples - folders in a directory
+
+// Generic Tree 
+// It has a root node which has variable number of children and child nodes which does not have any children is called leaf node.
+// Terminology
+// Ancestor - all nodes before the current node in euler path.
+// Descendant - the subtree beneath the current node.
+// Parent - The node immediately before the current node.
+// child - The node immediately after the current node.
+// Root - Node which has no parent.
+
 import java.io.*;
+import java.util.*;
 
 public class Main {
 
-    public static class Node {
+    private static class Node {
         int data;
         ArrayList<Node> children = new ArrayList<>();
     }
 
-    public static class GenericTree {
-        Node root;
+    public static Node constructGenericTree(int[] arr) {
+        Node root = null;
+        Stack<Node> stack = new Stack<>();
+        for (int i = 0; i < arr.length; i++) {
 
-        Node constructGenericTree(int[] arr) {
-            Stack<Node> stack = new Stack<>();
-            for (int i = 0; i < arr.length; i++) {
-                if (arr[i] == -1) {
-                    stack.pop();
+            if (arr[i] == -1) {
+                stack.pop();
+            } else {
+
+                Node temp = new Node();
+                temp.data = arr[i];
+
+                if (stack.isEmpty()) {
+                    root = temp;
                 } else {
-                    Node n = new Node();
-                    n.data = arr[i];
-
-                    if (stack.size() == 0) {
-                        this.root = n;
-                    } else {
-                        stack.peek().children.add(n);
-                    }
-                    stack.push(n);
+                    stack.peek().children.add(temp);
                 }
+                stack.push(temp);
             }
-            return root;
         }
+        return root;
+    }
 
-        public void display(Node node) {
-            StringBuilder branch = new StringBuilder(node.data + " -> ");
-            for (int i = 0; i < node.children.size(); i++) {
-                Node child = node.children.get(i);
-                branch.append(child.data + ", ");
-            }
-            System.out.println(branch);
-            for (int i = 0; i < node.children.size(); i++) {
-                Node child = node.children.get(i);
-                display(child);
-            }
+    public static void displayGenericTree(Node node){
+        StringBuilder sb = new StringBuilder(new String(node.data + " -> "));
+        for(Node child: node.children){
+            sb.append(child.data + ", ");
+        }
+        sb.append(".");
+        System.out.println(sb);
+        for(Node child: node.children){
+            displayGenericTree(child);
         }
     }
 
-    public static void main(String[] args) {
-        // Scanner scn = new Scanner(System.in);
-        // int arr[] = new int[scn.nextInt()];
-        // for (int i = 0; i < arr.length; i++) {
-        // arr[i] = scn.nextInt();
+    public static void main(String[] args) { // Driver Program
+
+        // BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        // int n = Integer.parseInt(br.readLine());
+        // int[] treeNodesInfo = new int[n];
+        // for (int i = 0; i < treeNodesInfo.length; i++) {
+        // treeNodesInfo[i] = Integer.parseInt(br.readLine());
         // }
-        int[] arr = { 10, 12, -1, 14, 18, -1, 20, 22, -1, 24, -1, 26, -1, -1, -1, 16, 28, -1, -1, -1 };
-        GenericTree tree = new GenericTree();
-        Node root = tree.constructGenericTree(arr);
-        tree.display(root);
+        int[] treeNodesInfo = { 10, 20, 50, -1, 60, -1, -1, 30, 70, -1, 80, 110, -1, 120, -1, -1, 90, -1, -1, 40, 100,
+                -1, -1, -1 };
+        Node root = constructGenericTree(treeNodesInfo);
+        displayGenericTree(root);
     }
 }
