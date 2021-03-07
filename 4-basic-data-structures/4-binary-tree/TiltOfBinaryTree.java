98958c5a7017c185015279c13399c68d09536d56
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-03-07 16:42:19 +0530

    Tilt of a binary tree added

--- 4-basic-data-structures/4-binary-tree/TiltOfBinaryTree.java
+++ 4-basic-data-structures/4-binary-tree/TiltOfBinaryTree.java
@@ -91,11 +91,22 @@ public class TiltOfBinaryTree {
         return th;
     }
 
+    
     static int tilt = 0;
-
     public static int tilt(Node node) {
         // write your code here to set the tilt data member
-        
+        if(node == null){
+            return 0;
+        }
+
+        int ls = tilt(node.left);
+        int rs = tilt(node.right);
+
+        int localTilt = Math.abs(ls-rs);
+        tilt += localTilt;
+
+        int sum = ls + rs + node.data;
+        return sum;
     }
 
     public static void main(String[] args) throws Exception {
