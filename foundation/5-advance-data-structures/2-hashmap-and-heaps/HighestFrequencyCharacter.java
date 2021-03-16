bb7d9a5c86de41979f3178a6eb0c73aac9dbe664
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-03-16 21:22:42 +0530

    added code for highest frequency character using hashmap

--- foundation/5-advance-data-structures/2-hashmap-and-heaps/HighestFrequencyCharacter.java
+++ foundation/5-advance-data-structures/2-hashmap-and-heaps/HighestFrequencyCharacter.java
@@ -1,16 +1,33 @@
 import java.io.*;
 import java.util.*;
 
-import jdk.internal.org.jline.utils.InputStreamReader;
-
-public class Main {
+public class HighestFrequencyCharacter {
 
     public static void main(String[] args) throws Exception {
         // write your code here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = new String(br.readLine());
+        HashMap<Character, Integer> map = new HashMap<>();
+        int mf = 0;
+        char mfch = ' ';
 
-        
+        for (int i = 0; i < str.length(); i++) {
+            char ch = str.charAt(i);
+
+            if (map.containsKey(ch) == true) {
+                int prevCount = map.get(ch);
+                int currCount = prevCount + 1;
+                map.put(ch, currCount);
+            } else {
+                map.put(ch, 1);
+            }
+
+            if (mf < map.get(ch)) {
+                mf = map.get(ch);
+                mfch = ch;
+            }
+        }
+        System.out.println(mfch);
     }
 
 }
