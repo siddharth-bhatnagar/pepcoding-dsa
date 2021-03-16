6e1f6ae259d3ebfbe1e68db7570e75fc67dc793d
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-03-16 20:52:19 +0530

    leetcode 128 added

---
+++ foundation/5-advance-data-structures/2-hashmap-and-heaps/HighestFrequencyCharacter.java
@@ -0,0 +1,16 @@
+import java.io.*;
+import java.util.*;
+
+import jdk.internal.org.jline.utils.InputStreamReader;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        // write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = new String(br.readLine());
+
+        
+    }
+
+}
---
+++ foundation/5-advance-data-structures/2-hashmap-and-heaps/LongestConsecutiveSequence.java
@@ -0,0 +1,45 @@
+import java.util.Scanner;
+
+public class LongestConsecutiveSequence {
+    public static void Main(String[] args) {
+
+        Scanner scn = new Scanner(System.in);
+        int[] nums = new int[scn.nextInt()];
+
+        for (int i = 0; i < nums.length; i++) {
+            nums[i] = scn.nextInt();
+        }
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int max = 0;
+
+        for (int i = 0; i < nums.length; i++) {
+
+            int num = nums[i];
+
+            if (map.containsKey(num) == false) {
+                int sp = num;
+                int ep = num;
+
+                if (map.containsKey(num - 1) == true) {
+                    sp = sp - map.get(num - 1);
+                }
+
+                if (map.containsKey(num + 1) == true) {
+                    ep = ep + map.get(num + 1);
+                }
+
+                int newlen = ep - sp + 1;
+                map.put(sp, newlen);
+                map.put(ep, newlen);
+
+                if (sp != num && ep != num) {
+                    map.put(num, 1);
+                }
+
+                max = Math.max(max, newlen);
+            }
+        }
+        System.out.println(max);
+    }
+}
