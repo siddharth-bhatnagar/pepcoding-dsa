064c8d3a1223632dcc9cabded6aebb823b34bdc3
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-04-14 17:30:46 +0530

    added leetcode 747

--- level-up-and-interview-prep/1-arrays/LargestNumberAtLeastTwiceOfOthers.java
+++ level-up-and-interview-prep/1-arrays/LargestNumberAtLeastTwiceOfOthers.java
@@ -1,1 +1,24 @@
 // Leetcode 747
+class Solution {
+    public int dominantIndex(int[] nums) {
+        int largest = Integer.MIN_VALUE;
+        int secondLargest = Integer.MIN_VALUE;
+        int idx = 0;
+        for (int i = 0; i < nums.length; i++) {
+            int num = nums[i];
+            if (num > largest) {
+                secondLargest = largest;
+                largest = num;
+                idx = i;
+            } else if (num > secondLargest) {
+                secondLargest = num;
+            }
+        }
+
+        if (largest >= 2 * secondLargest) {
+            return idx;
+        } else {
+            return -1;
+        }
+    }
+}
