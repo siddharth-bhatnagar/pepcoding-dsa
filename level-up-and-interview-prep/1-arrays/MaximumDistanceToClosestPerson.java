207768e733ebf7d6555635b2b1b4265bdd466c0f
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-04-15 22:30:52 +0530

    Leetcode 849

--- level-up-and-interview-prep/1-arrays/MaximumDistanceToClosestPerson.java
+++ level-up-and-interview-prep/1-arrays/MaximumDistanceToClosestPerson.java
@@ -1,2 +1,30 @@
 // Leetcode 849
 
+class Solution {
+    public int maxDistToClosest(int[] nums) {
+        int ans;
+        int i, j;
+        i = 0;
+        while (nums[i] != 1) {
+            i++;
+        }
+        ans = i;
+        j = i + 1;
+
+        while (j < nums.length) {
+            if (nums[j] == 0 && j == nums.length - 1) {
+                ans = Math.max(ans, j - i);
+                return ans;
+            } else if (nums[j] == 0) {
+                j++;
+            } else if (nums[j] == 1) {
+                int mid = (j - i) / 2;
+                ans = Math.max(ans, mid);
+                i = j;
+                j = i + 1;
+            }
+        }
+
+        return ans;
+    }
+}
