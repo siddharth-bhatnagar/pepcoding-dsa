215c74d19107a755684361efb7afad711fe2850d
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-05-05 15:29:32 +0530

    maximum swap and reach a number

--- level-up-and-interview-prep/1-arrays/MaximumSwap.java
+++ level-up-and-interview-prep/1-arrays/MaximumSwap.java
@@ -1,2 +1,28 @@
 // Leetcode 670
-
+// O(n) time O(n) space approach using suffixmax array
+class Solution {
+    public int maximumSwap(int num) {
+        char[] digits = (num + "").toCharArray();
+        int n = digits.length;
+        int[] suffixmax = new int[n];
+        suffixmax[n-1] = (int)digits[n-1];
+        for(int i=n-2;i>=0;i--) {
+            suffixmax[i] = Math.max(suffixmax[i+1], digits[i]);
+        }
+        
+        for(int i=0;i<n;i++) {
+            if(digits[i] < suffixmax[i]) {
+                for(int j=n-1;j>=i+1;j--){
+                    if(digits[j]==suffixmax[i]){
+                        char temp = digits[i];
+                        digits[i] = digits[j];
+                        digits[j] = temp;
+                        break;
+                    }
+                }
+                break;
+            }
+        }
+        return Integer.parseInt(new String(digits));
+    }
+}
--- level-up-and-interview-prep/1-arrays/ReachANumber.java
+++ level-up-and-interview-prep/1-arrays/ReachANumber.java
@@ -1,2 +1,30 @@
 // Leetcode 754
 // GFG - Number of jumps to reach X
+
+class Solution {
+    public int reachNumber(int target) {
+        target = Math.abs(target);
+        int moves = 0;
+        int i = 1;
+        int val = 0;
+        while (true) {
+            if (val == target) {
+                break;
+            } else if (val < target) {
+                val += i;
+                i++;
+                moves++;
+            } else if (val > target) {
+                int d = val - target;
+                if (d % 2 == 0) {
+                    break;
+                }
+                val += i;
+                i++;
+                moves++;
+            }
+        }
+
+        return moves;
+    }
+}
