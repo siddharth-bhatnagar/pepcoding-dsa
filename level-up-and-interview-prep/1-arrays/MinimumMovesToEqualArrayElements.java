7cd2eca885b2ddb8f36bd6c89722c7aaa08bd236
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-05-05 13:06:18 +0530

    Minimum moves to equal array elements II : Leetcode 462

--- level-up-and-interview-prep/1-arrays/MinimumMovesToEqualArrayElements.java
+++ level-up-and-interview-prep/1-arrays/MinimumMovesToEqualArrayElements.java
@@ -1,1 +1,18 @@
-// Leetcode 462
+// Leetcode 462 
+// Minimum moves to equal array elements two
+
+// Strategy, all array elements should become equal to the median
+
+class Solution {
+    public int minMoves2(int[] nums) {
+
+        Arrays.sort(nums);
+        int median = nums[nums.length / 2];
+        int moves = 0;
+        for (int i = 0; i < nums.length; i++) {
+            moves += Math.abs(median - nums[i]);
+        }
+
+        return moves;
+    }
+}
---
+++ level-up-and-interview-prep/1-arrays/ReachANumber.java
@@ -0,0 +1,2 @@
+// Leetcode 754
+// GFG - Number of jumps to reach X
