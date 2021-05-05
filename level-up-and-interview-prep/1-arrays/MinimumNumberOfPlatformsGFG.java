5163fee0e551f673a6de6243ea5ec58ef3abfb6d
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-05-05 14:01:58 +0530

    update

--- level-up-and-interview-prep/1-arrays/MinimumNumberOfPlatformsGFG.java
+++ level-up-and-interview-prep/1-arrays/MinimumNumberOfPlatformsGFG.java
@@ -1,41 +1,6 @@
 import java.util.*;
 
-class GFG {
-
-    // Returns minimum number of platforms reqquired
-    static int findPlatform(int arr[], int dep[], int n) {
-        // Sort arrival and departure arrays
-        Arrays.sort(arr);
-        Arrays.sort(dep);
-
-        // plat_needed indicates number of platforms
-        // needed at a time
-        int plat_needed = 1, result = 1;
-        int i = 1, j = 0;
-
-        // Similar to merge in merge sort to process
-        // all events in sorted order
-        while (i < n && j < n) {
-            // If next event in sorted order is arrival,
-            // increment count of platforms needed
-            if (arr[i] <= dep[j]) {
-                plat_needed++;
-                i++;
-            }
-
-            // Else decrement count of platforms needed
-            else if (arr[i] > dep[j]) {
-                plat_needed--;
-                j++;
-            }
-
-            // Update result if needed
-            if (plat_needed > result)
-                result = plat_needed;
-        }
-
-        return result;
-    }
+public class MinimumNumberOfPlatformsGFG {
 
     // Driver code
     public static void main(String[] args) {
@@ -43,5 +8,24 @@ class GFG {
         int dep[] = { 910, 1200, 1120, 1130, 1900, 2000 };
         int n = arr.length;
         System.out.println("Minimum Number of Platforms Required = " + findPlatform(arr, dep, n));
+    }
+
+    // Returns minimum number of platforms reqquired
+    public static int findPlatform(int arr[], int dep[], int n) {
+        Arrays.sort(arr);
+        Arrays.sort(dep);
+        int i = 0, j = 0, platform = 0, ans = Integer.MIN_VALUE;
+        while (i < n && j < n) {
+            if (arr[i] <= dep[j]) {
+                platform++;
+                ans = Math.max(ans, platform);
+                i++;
+            } else if (arr[i] > dep[j]) {
+                j++;
+                platform--;
+            }
+        }
+
+        return ans;
     }
 }
