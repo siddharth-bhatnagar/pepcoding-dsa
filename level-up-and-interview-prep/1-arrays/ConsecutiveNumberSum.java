8024df4a35be936b672c215ae6626f68950d9d56
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-05-05 18:58:13 +0530

    consecutive number sum : Leetcode 829

--- level-up-and-interview-prep/1-arrays/ConsecutiveNumberSum.java
+++ level-up-and-interview-prep/1-arrays/ConsecutiveNumberSum.java
@@ -1,3 +1,16 @@
-public class ConsecutiveNumberSum {
-    
+// Leetcode 829
+// O(N^1/2) time, O(1) space
+
+class Solution {
+    public int consecutiveNumbersSum(int N) {
+        int count = 0;
+        for (int k = 1; k * k < (2 * N); k++) {
+            int div = N - (k * (k - 1)) / 2;
+            if (div % k == 0) {
+                count++;
+            }
+        }
+
+        return count;
+    }
 }
