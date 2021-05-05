6c57a2962a0ca1b10dec8b792539b23f30a82931
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-05-05 15:33:28 +0530

    update

--- level-up-and-interview-prep/1-arrays/MaximumSwap.java
+++ level-up-and-interview-prep/1-arrays/MaximumSwap.java
@@ -26,3 +26,28 @@ class Solution {
         return Integer.parseInt(new String(digits));
     }
 }
+
+// O(N) time , O(1) space
+class Solution {
+    public int maximumSwap(int num) {
+        char[] digits = Integer.toString(num).toCharArray();
+        
+        int[] buckets = new int[10];
+        for (int i = 0; i < digits.length; i++) {
+            buckets[digits[i] - '0'] = i;
+        }
+        
+        for (int i = 0; i < digits.length; i++) {
+            for (int k = 9; k > digits[i] - '0'; k--) {
+                if (buckets[k] > i) {
+                    char tmp = digits[i];
+                    digits[i] = digits[buckets[k]];
+                    digits[buckets[k]] = tmp;
+                    return Integer.valueOf(new String(digits));
+                }
+            }
+        }
+        
+        return num;
+    }
+}
