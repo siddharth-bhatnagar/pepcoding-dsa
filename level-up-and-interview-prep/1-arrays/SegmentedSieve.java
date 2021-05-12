958dbb3342dd56dc2f879dd54c6f89f7534e3d7a
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-05-12 18:44:47 +0530

    added code for segmented sieve

--- level-up-and-interview-prep/1-arrays/SegmentedSieve.java
+++ level-up-and-interview-prep/1-arrays/SegmentedSieve.java
@@ -64,3 +64,67 @@ public class Main {
         return result;
     }
 }
+
+
+
+// ------------------------------------------------- Passing on SPOJ below---------------------------------------------//
+import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+ 
+public class Main{
+ 
+    
+public static void main(String[] args) throws NumberFormatException,IOException{
+   
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int t = Integer.parseInt(br.readLine());
+		while(t-- > 0){
+			String []str = br.readLine().split(" ");
+			long n = Long.parseLong(str[0]);
+			long m = Long.parseLong(str[1]);
+		    int k = (int)Math.sqrt(m);
+		    boolean []a = new boolean[k+1];
+			for(int i=2;i*i<=m;i++){
+			    if(a[i]==false){
+			        for(int j=i*i;j<=k;j=j+i){
+			            a[j] = true;
+			        }
+			    }
+			}
+			ArrayList<Integer> prime = new ArrayList<>();
+			for(int i=2;i<a.length;i++){
+			    if(a[i]==false){
+			        prime.add(i);
+			    }
+			}
+// 			System.out.println(prime);
+			int span = (int)(m-n);
+			boolean []ans = new boolean[span+1];
+			for(int i=0;i<prime.size();i++){
+			    double p = (double)n/(double)prime.get(i);
+			    int start = (int)((Math.ceil(p)) * prime.get(i));
+			    
+			    if(start == prime.get(i)){
+			        start = start + prime.get(i);
+			    }
+			 //   System.out.println(start);
+			    for(int j=start ; j<=m ; j = j+prime.get(i)){
+			        ans[(int)(j-n)] = true;
+			    }
+			 
+			 //System.out.println(Math.ceil(p));
+			}
+			long s = n;
+			if(s==(long)1){
+			    s=2;
+			}
+			for(int i=(int)s ; i<=m ; i++){
+			    if(ans[(int)(i - n)]==false){
+			        System.out.println(i);
+			    } 
+			}
+		}
+    }
+}
