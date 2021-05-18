b7484bb405487b5287e8ce0e731ec1a4468cdef1
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-05-19 00:37:48 +0530

    max consecutive ones 1

---
+++ level-up-and-interview-prep/1-arrays/MaxConsecutiveOnes.java
@@ -0,0 +1,34 @@
+// Leetcode 485
+
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int maxSum = 0, sum = 0;
+        for (int n : nums) {
+            sum *= n;
+            sum += n;
+            maxSum = Math.max(maxSum, sum);
+        }
+        return maxSum;
+    }
+}
+
+// O(n), O(1)
+
+/**
+public class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+        int result = 0;
+        int count = 0;
+        
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] == 1) {
+        	count++;
+        	result = Math.max(count, result);
+            }
+            else count = 0;
+        }
+        
+        return result;
+    }
+}
+ */
