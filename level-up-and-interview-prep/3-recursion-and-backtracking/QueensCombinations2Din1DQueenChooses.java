9a2d9c069b9bc8b53358608385d7e0e9853d22ba
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-05-19 19:03:43 +0530

    added queens combination in 2d as 1d, queen chooses

---
+++ level-up-and-interview-prep/3-recursion-and-backtracking/QueensCombinations2Din1DQueenChooses.java
@@ -0,0 +1,41 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void queensCombinations(int qpsf, int tq, boolean[][] chess, int lcno) {
+        // write your code here
+        if (qpsf == tq) {
+            for (int i = 0; i < chess.length; i++) {
+                for (int j = 0; j < chess[0].length; j++) {
+                    if (chess[i][j] == true) {
+                        System.out.print("q\t");
+                    } else {
+                        System.out.print("-\t");
+                    }
+                }
+                System.out.println();
+            }
+            System.out.println();
+            return;
+        }
+        
+        for (int cell = lcno + 1; cell < tq * tq; cell++) {
+            int row = cell / chess[0].length;
+            int col = cell % chess[0].length;
+            if (chess[row][col] == false) {
+                chess[row][col] = true;
+                queensCombinations(qpsf + 1, tq, chess, cell);
+                chess[row][col] = false;
+            }
+        }
+    }
+
+    public static void main(String[] args) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        boolean[][] chess = new boolean[n][n];
+
+        queensCombinations(0, n, chess, -1);
+    }
+}
