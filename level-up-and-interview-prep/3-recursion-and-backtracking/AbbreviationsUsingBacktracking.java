4a173a1811d1b8721a5581151c8f1ff3aea92174
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-05-31 19:41:44 +0530

    Abbreviations using backtracking

---
+++ level-up-and-interview-prep/3-recursion-and-backtracking/AbbreviationsUsingBacktracking.java
@@ -0,0 +1,34 @@
+import java.util.*;
+
+public class AbbreviationsUsingBacktracking {
+
+    public static void solution(String str, String asf, int count, int pos) {
+        
+        if(pos == str.length()) {
+            if(count ==0) {
+                System.out.println(asf);
+            }
+            else {
+                System.out.println(asf+count);
+            }
+
+            return;
+        }
+
+        char ch = str.charAt(pos);
+        if(count > 0) {
+            solution(str, asf + count + ch, 0, pos + 1);
+        }
+        else {
+            solution(str, asf + ch, 0, pos + 1);
+        }
+        solution(str, asf, count + 1, pos + 1);
+    }
+
+    public static void main(String[] args) {
+        Scanner scn = new Scanner(System.in);
+        String str = scn.nextLine();
+        solution(str, "", 0, 0);
+        scn.close();
+    }
+}
