f48efa087a9a0b346eff92547573e83e88f7ff20
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-06-16 13:42:27 +0530

    Tower of Hanoi

--- foundation/2-recursion-and-backtracking/1-introduction-to-recursion/TowerOfHanoi.java
+++ foundation/2-recursion-and-backtracking/1-introduction-to-recursion/TowerOfHanoi.java
@@ -4,11 +4,21 @@ import java.util.*;
 public class TowerOfHanoi {
 
     public static void main(String[] args) throws Exception {
-        
+        Scanner scn = new Scanner(System.in);
+        int n = scn.nextInt();
+        int t1 = scn.nextInt();
+        int t2 = scn.nextInt();
+        int t3 = scn.nextInt();
+        toh(n, t1, t2, t3);
+        scn.close();
     }
 
     public static void toh(int n, int t1id, int t2id, int t3id){
-        
+        if(n==0) return;
+
+        toh(n-1, t1id, t3id, t2id);
+        System.out.println(n + "[" + t1id + "->" + t2id + "]");
+        toh(n-1, t3id, t2id, t1id);
     }
 
 }
