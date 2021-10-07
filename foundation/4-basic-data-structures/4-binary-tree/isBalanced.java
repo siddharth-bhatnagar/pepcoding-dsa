b60fe82f4ad63e8efbc6db813d7789069209c7cd
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-10-07 15:49:30 +0530

    update

---
+++ foundation/4-basic-data-structures/4-binary-tree/isBalanced.java
@@ -0,0 +1,43 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+
+//  Leetcode 110
+class isBalanced {
+    boolean balanced;
+    public boolean isBalanced(TreeNode root) {
+        balanced = true;
+        helper(root);
+        return balanced;
+    }
+    
+    public int helper(TreeNode node) {
+        if(node == null) return 0;
+        if(balanced) {
+            int h = 0;
+            int lh = helper(node.left);
+            int rh = helper(node.right);
+            
+            int d = Math.abs(lh - rh);
+            if(d > 1) {
+                balanced = false;
+            }
+            
+            h = Math.max(lh, rh) + 1;
+            return h;
+        }
+        return -1;
+    }
+}
