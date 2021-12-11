b377fc35ca6c88c81e1a1f38b491615cbe19976b
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-12-11 21:10:28 +0530

    update

--- level-up-and-interview-prep/4-trees/5-misc/ClosestBSTValueTreeOne.java
+++ level-up-and-interview-prep/4-trees/5-misc/ClosestBSTValueTreeOne.java
@@ -1,36 +1,49 @@
 // Lintcode 900
 
 /**
- * Definition of TreeNode: public class TreeNode { public int val; public
- * TreeNode left, right; public TreeNode(int val) { this.val = val; this.left =
- * this.right = null; } }
+ * Definition of TreeNode:
+ * public class TreeNode {
+ *     public int val;
+ *     public TreeNode left, right;
+ *     public TreeNode(int val) {
+ *         this.val = val;
+ *         this.left = this.right = null;
+ *     }
+ * }
  */
 
 public class Solution {
     /**
-     * @param root:   the given BST
+     * @param root: the given BST
      * @param target: the given target
      * @return: the value in the BST that is closest to the target
      */
     public int closestValue(TreeNode root, double target) {
-        double gap = Double.MAX_VALUE;
-        int ans = -1;
+        if(root == null) return 0;
+        closest = root.val;
+        difference = Math.abs(target - root.val); 
+        findClosest(root, target);
+        return closest;
+    }
 
-        while (root != null) {
-            double dif = Math.abs(root.val - target);
-            if (dif <= gap) {
-                ans = root.val;
-                gap = dif;
-            }
+    private double difference;
+    private int closest;
 
-            if (root.val > target) {
-                root = root.left;
-            } else if (root.val < target) {
-                root = root.right;
-            } else {
-                return ans;
-            }
+    private void findClosest(TreeNode root, double target) {
+        if(root == null) return;
+        double gap = Math.abs(target - root.val);
+        if(gap < difference) {
+            difference = gap;
+            closest = root.val;
         }
-        return ans;
+
+        if(root.val == target) return;
+
+        if(root.val > target) {
+            findClosest(root.left, target);
+        }
+        else if(root.val < target) {
+            findClosest(root.right, target);
+        }
     }
 }
