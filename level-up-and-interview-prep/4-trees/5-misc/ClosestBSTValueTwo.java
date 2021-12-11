4ffdb19d5bdb551cc390e6fd9226874377d98458
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2021-12-11 22:48:04 +0530

    closest bst value ii using morris

--- level-up-and-interview-prep/4-trees/5-misc/ClosestBSTValueTwo.java
+++ level-up-and-interview-prep/4-trees/5-misc/ClosestBSTValueTwo.java
@@ -20,6 +20,7 @@ public class Solution {
         return ans;
     }
 
+    // recursive O(N) time, O(H + k) space
     public static void traversal(TreeNode node, double target, int k, LinkedList<Integer> ans) {
         if (node == null)
             return;
@@ -39,5 +40,62 @@ public class Solution {
                 return;
         }
         traversal(node.right, target, k, ans);
+    }
+}
+
+// O(N) time, O(k) space
+// Using Morris Inorder Traversal
+public class Solution {
+    public List<Integer> closestKValues(TreeNode root, double target, int k) {
+        LinkedList<Integer> window = new LinkedList<>();
+        if(root == null) {
+           window.addLast(0);
+           return window;
+        }
+
+        TreeNode curr = root;
+        while(curr != null) {
+            if(curr.left != null) {
+                TreeNode helper = curr.left;
+                while(helper.right != null && helper.right != curr) {
+                    helper = helper.right;
+                }
+
+                if(helper.right == null) {
+                    helper.right = curr;
+                    curr = curr.left;
+                }
+                else if(helper.right == curr) {
+                    if(window.size() < k) {
+                        window.addLast(curr.val);
+                    }
+                    else if(Math.abs(target - curr.val) < Math.abs(target - window.peekFirst())) {
+                        window.removeFirst();
+                        window.addLast(curr.val);
+                    }
+                    else {
+                        return window;
+                    }
+                    
+                    helper.right = null;
+                    curr = curr.right;
+                }
+            }
+            else {
+                if(window.size() < k) {
+                    window.addLast(curr.val);
+                }
+                else if(Math.abs(target - curr.val) < Math.abs(target - window.peekFirst())) {
+                    window.removeFirst();
+                    window.addLast(curr.val);
+                }
+                else {
+                    return window;
+                }
+                curr = curr.right;
+            }
+        }
+
+        return window;
     }
 }
