a5933fc6b10342b3f215ece27841df42664e48e9
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2022-01-19 21:29:48 +0530

    update

--- foundation/1-basics-of-programming/5-string-stringbuilder-arraylist/PallindromicSubstring.java
+++ foundation/1-basics-of-programming/5-string-stringbuilder-arraylist/PallindromicSubstring.java
@@ -26,3 +26,33 @@ public class PallindromicSubstring {
         solution(str);
     }
 }
+
+// Leetcode 647
+// O(n3) solution
+
+class Solution {
+    public int countSubstrings(String s) {
+        int count = 0;
+        for(int i=0;i<s.length();i++) {
+            for(int j=i;j<s.length();j++) {
+                count += isPallindrome(s, i, j) ? 1 : 0;
+            }
+        }
+        
+        return count;
+    }
+    
+    private boolean isPallindrome(String s, int si, int ei) {
+        while(si <= ei) {
+            char ch1 = s.charAt(si);
+            char ch2 = s.charAt(ei);
+            
+            if(ch1 != ch2) return false;
+            
+            si++;
+            ei--;
+        }
+        
+        return true;
+    }
+}
