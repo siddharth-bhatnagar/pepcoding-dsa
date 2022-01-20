e46f0f1b4d0ae481b63e085586f5ca9a8515dc45
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2022-01-20 17:43:07 +0530

    leetcode 443

---
+++ level-up-and-interview-prep/2-strings/StringCompression.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int compress(char[] chars) {
+        int length = 0, index = 0, n = chars.length;
+        
+        while(index < n) {
+            char curr = chars[index];
+            int count = 0;
+            while(index < n && chars[index] == curr) {
+                count++;
+                index++;
+            }
+
+            chars[length] = curr;
+            length++;
+            if(count > 1) {
+                for(char c: Integer.toString(count).toCharArray()) {
+                    chars[length] = c;
+                    length++;
+                }
+            }
+        }
+
+        return length;
+    }
+}
