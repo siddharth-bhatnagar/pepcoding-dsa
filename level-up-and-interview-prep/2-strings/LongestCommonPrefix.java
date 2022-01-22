af7c7d79a4e8bba90b7cbadb7f77d2d1feb81c9e
siddharth-bhatnagar <siddharth.bhatnagar.msit@iosd.tech>
2022-01-22 15:56:18 +0530

    update

--- level-up-and-interview-prep/2-strings/LongestCommonPrefix.java
+++ level-up-and-interview-prep/2-strings/LongestCommonPrefix.java
@@ -3,17 +3,18 @@
 // O(M*N) where M is the number of strings, N is length of Longest String
 
 class Solution {
-    public String longestCommonPrefix(String[] strs) {
-        if (strs.length == 0)
-            return "";
-        String lcp = strs[0];
-        for (int i = 1; i < strs.length; i++) {
-            String s = strs[i];
-            while (s.indexOf(lcp) != 0) {
-                lcp = lcp.substring(0, lcp.length() - 1);
-            }
+    public String longestCommonPrefix(String[] arr) {
+        if(arr.length == 0) return "";
+        int n = arr.length;
+        String str = arr[0];
+        for(int i=1;i<n;i++) {
+            String s = arr[i];
+            int m = s.length(), k = str.length();
+            int j = 0;
+            while(j < m && j < k && str.charAt(j) == s.charAt(j)) j++;
+            str = str.substring(0, j);
         }
-
-        return lcp;
+        
+        return str;
     }
 }
